4e00ff371308781ac1b1d5c26dde35479b59d655
ManishAwhade <manish.awhade>
2023-11-22 18:41:23 +0530

    cardCarouselNewAlbums-qtify-takehome

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -7,9 +7,16 @@ const BASE_URL = "https://qtify-backend-labs.crio.do";
 
 const Home = () => {
   const [topAlbum, settopAlbum] = useState(null);
+  const [newAlbum, setnewAlbum] = useState(null);
+  const [songs, setsongs] = useState(null);
+  const [genres, setgenres] = useState(null);
 
   useEffect(() => {
     fetchTopAlbums();
+    fetchNewAlbums();
+    // // fetchAlbumDetail()
+    // fetchSongs();
+    // fetchGenres();
   }, []);
 
   const fetchTopAlbums = () => {
@@ -20,11 +27,44 @@ const Home = () => {
       })
       .catch((err) => {});
   };
+  const fetchNewAlbums = () => {
+    axios
+      .get(`${BASE_URL}/albums/new`)
+      .then((result) => {
+        setnewAlbum(result.data);
+      })
+      .catch((err) => {});
+  };
+  const fetchAlbumDetail = (slug) => {
+    axios
+      .get(`${BASE_URL}/albums/${slug}`)
+      .then((result) => {
+        setnewAlbum(result.data);
+      })
+      .catch((err) => {});
+  };
+  const fetchSongs = () => {
+    axios
+      .get(`${BASE_URL}/songs`)
+      .then((result) => {
+        setsongs(result.data);
+      })
+      .catch((err) => {});
+  };
+  const fetchGenres = () => {
+    axios
+      .get(`${BASE_URL}/genres`)
+      .then((result) => {
+        setgenres(result.data);
+      })
+      .catch((err) => {});
+  };
 
   return (
     <div style={{ padding: "0 40px 0 40px" }}>
       <>
         <AlbumsComp title={"Top Albums"} data={topAlbum} />
+        <AlbumsComp title={"New Albums"} data={newAlbum} />
       </>
     </div>
   );
